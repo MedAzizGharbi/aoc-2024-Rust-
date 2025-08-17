d1234e567da1653922c14fc434efddd36c897452
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2025-08-17 23:32:29 +0200

    Day2 part2 + Cleanup

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -3,41 +3,63 @@ use std::fs;
 pub fn run() {
     let path = "inputs/day02.txt";
     let data = fs::read_to_string(path).unwrap();
-    println!("Part 1 => {}", part1(data));
+    println!("Part 1 => {}", part1(&data));
+    println!("Part 2 => {}", part2(&data));
 }
 
-fn part1(input: String) -> i32 {
-    let mut result = 0;
-    for line in input.lines() {
-        let report: Vec<i32> = line
-            .split_whitespace()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect();
-        let n = report.len();
-        let mut valid = true;
-        if report[1] > report[0] {
-            for i in 1..n {
-                let diff = report[i] - report[i - 1];
-                if !(diff > 0 && diff <= 3) {
-                    valid = false;
-                    break;
+fn part1(input: &String) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let report: Vec<i32> = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect();
+            if is_safe(&report) { 1 } else { 0 }
+        })
+        .sum()
+}
+fn part2(input: &String) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let report: Vec<i32> = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect();
+            if is_safe(&report) {
+                return 1;
+            } else {
+                for i in 0..report.len() {
+                    let mut reduced = report.clone();
+                    reduced.remove(i);
+                    if is_safe(&reduced) {
+                        return 1;
+                    }
                 }
             }
-            if valid {
-                result += 1;
+            0
+        })
+        .sum()
+}
+fn is_safe(report: &[i32]) -> bool {
+    if report.len() < 2 {
+        return true;
+    }
+    let increasing = report[1] > report[0];
+
+    for i in 1..report.len() {
+        let diff = report[i] - report[i - 1];
+
+        if increasing {
+            if !(diff > 0 && diff <= 3) {
+                return false;
             }
-        } else if report[1] < report[0] {
-            for i in 1..n {
-                let diff = report[i] - report[i - 1];
-                if !(diff < 0 && diff >= -3) {
-                    valid = false;
-                    break;
-                }
-            }
-            if valid {
-                result += 1;
+        } else {
+            if !(diff < 0 && diff >= -3) {
+                return false;
             }
         }
     }
-    result
+    true
 }
