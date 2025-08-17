590102279f1f9be9bb3d1f59399af5efa38a533f
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2025-08-17 19:48:06 +0200

    Day 1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,7 +1,9 @@
+use std::collections::{HashMap, HashSet};
 use std::fs;
 pub fn run() {
     let input = "inputs/day01.txt";
     println!("Part1: {}", part1(&input));
+    println!("Part2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
@@ -18,6 +20,23 @@ fn part1(input: &str) -> i32 {
     let mut result: i32 = 0;
     for i in 0..x.len() {
         result += (x[i] - y[i]).abs();
+    }
+    result
+}
+
+fn part2(input: &str) -> i32 {
+    let data = fs::read_to_string(input).unwrap();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut right = Vec::new();
+    for line in data.lines() {
+        let v: Vec<&str> = line.split("   ").collect();
+        right.push(v[0].parse::<i32>().unwrap());
+        *map.entry(v[1].parse::<i32>().unwrap()).or_insert(0) += 1;
+    }
+    let mut result = 0;
+    for i in right {
+        let x = map.get(&i).unwrap_or(&0);
+        result += i * x;
     }
     result
 }
