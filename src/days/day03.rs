726d11d8191fafedad0326939bfce843e9577387
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2025-08-18 00:11:58 +0200

    Day 3 pArT 1

---
+++ src/days/day03.rs
@@ -0,0 +1,33 @@
+use regex::Regex;
+use std::fs;
+pub fn run() {
+    let input = "inputs/day03.txt";
+    println!("Part1: {}", part1(&input));
+    // println!("Part2: {}", part2(&input));
+}
+
+fn part1(input: &str) -> i32 {
+    let data = fs::read_to_string(input).unwrap();
+
+    // Brief explanation of the regex
+    // mul\( → literally matches mul(.
+    //
+    // (\d{1,3}) → first capture group: matches 1 to 3 digits
+    //
+    // , → literal comma between numbers.
+    //
+    // (\d{1,3}) → second capture group: another 1–3 digit number.
+    //
+    // \) → literal closing parenthesis.
+
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    re.captures_iter(&data)
+        .map(|cap| {
+            let x: i32 = cap[1].parse().unwrap();
+            let y: i32 = cap[2].parse().unwrap();
+            x * y
+        })
+        .sum()
+}
+
+// fn part2(input: &str) -> i32 {}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     match day.as_str() {
         "1" => days::day01::run(),
         "2" => days::day02::run(),
+        "3" => days::day03::run(),
         _ => println!("Pick a valid date!"),
     }
 }
